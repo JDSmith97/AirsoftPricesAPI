42370a58e56af9dcc5996c32ed2a81935d422e51
Jack Smith <jjsmith1116@gmail.com>
2020-05-27 00:42:43 +0100

    Fixed CORS issue for API, started frontend

--- src/pages/item/index.jsx
+++ src/pages/item/index.jsx
@@ -1,41 +1,73 @@
 import React, { Component } from 'react'
-import { Button, Container, Row, Col, Card, CardBody, CardTitle, CardText, CardSubtitle } from 'reactstrap'
+import { Button, Container, Row, Col, Card, CardBody, CardTitle, CardText, CardSubtitle, Table } from 'reactstrap'
+import axios from 'axios'
 // import './index.scss'
 
 class Item extends Component {
 
-	constructor() {
-    super()
-    this.onFormSubmit = this.onFormSubmit.bind(this)
-    this.state = {
-      city: ''
-    }
+  state = {
+    item: [],
+    itemPrices: []
   }
 
-	onFormSubmit() {
-    if(!this.state.city){
-    	console.log("nope")
-    }
-	}
+  componentDidMount() {
+    axios.get(`https://3eg3r872u3.execute-api.eu-west-2.amazonaws.com/staging?id=38`)
+      .then(res => {
+        const itemInfo = res.data;
+        console.log(itemInfo)
+        this.setState({ item: itemInfo });
+      })
+  }
 
   render() {
+    let itemPrices = []
+    this.state.item.map(item => {
+      if(!item.patrol_base_price == ""){
+        itemPrices.push(
+          <tr key="PatrolBase">
+            <th scope="row" key="patrolBaseName">Patrol Base</th>
+            <td key="patrolBasePrice">{item.patrol_base_price}</td>
+            <td key="patrolBaseStock">{item.patrol_base_stock}</td>
+          </tr>
+        )
+      }
+      if(!item.surplus_store_price == ""){
+        itemPrices.push(
+          <tr key="SurplusStore">
+            <th scope="row" key="surplusName">Surplus Store</th>
+            <td key="surplusPrice">{item.surplus_store_price}</td>
+            <td key="surplusStock">{item.surplus_store_stock}</td>
+          </tr>
+        )
+      }
+      console.log(itemPrices)
+    })
+
     return (
-    	<Container>
-	      <Row className="">
-	        <Col className="text-center"></Col>
-	        <Col className="text-center">
-            <Card>
-              <CardBody>
-                <CardTitle>Card title</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
-                <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                <Button>Button</Button>
-              </CardBody>
-            </Card>
-	        </Col>
-	        <Col className="text-center"></Col>
-	      </Row>
-      </Container>
+      <React.Fragment>
+        {this.state.item.map(item => (
+          <Container>
+            <Row className="">
+              <Col className="text-center"></Col>
+              <Col className="text-center ">
+              <Table key={item.item_id}>
+                <thead>
+                  <tr>
+                    <th>Airsoft Store</th>
+                    <th>Price</th>
+                    <th>Stock Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {itemPrices}
+                </tbody>
+              </Table>
+              </Col>
+              <Col className="text-center"></Col>
+            </Row>
+          </Container>
+        ))}
+      </React.Fragment>
     );
   }
 }
--- website-api/index.js
+++ website-api/index.js
@@ -4,14 +4,17 @@ module.exports.handler = async function (event, context, callback) {
   try {
     let id = event.queryStringParameters.id
     const itemPrices = await getItemPrices.getPrices(id)
-    context.succeed({
-      "statusCode": 200,
-        "headers": {
-          "Content-Type": "application/json"
-        },
-        "body": JSON.stringify(itemPrices),
-        "isBase64Encoded": false
-    });
+
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*",
+        "Access-Control-Allow-Credentials" : true
+      },
+      body: JSON.stringify(itemPrices)
+    };
+
+    callback(null, response);
   }
   catch(err) {
     console.log(err)
